c1a3e81a815c30e7d0d3030c0c76d392dc276801
Chinmay <makhandi.chinmay@gmail.com>
2022-11-03 20:22:11 +0530

    Added Decorator with Parameters Check logs_decorator.py

--- py_decorators/log_decorator.py
+++ py_decorators/log_decorator.py
@@ -1,14 +1,16 @@
 import time
 
-def log_generate(func):
+def log_generate(log_file):
+  def logging(func):
     def wrapper(*args, **kwargs):
-        with open("logs.txt",'a') as l:
+        with open(log_file,'a') as l:
             l.write(f"Called Function {func.__name__} with arguments ({','.join([str(arg) for arg in args])}) at {time.strftime('%H:%M%p %Z on %b %d, %Y')} \n")
         func(*args, **kwargs)
     return wrapper
+  return logging
 
-@log_generate
+@log_generate(log_file="/home/chinmay/python_course/my_handy_codes/py_decorators/logs.txt")
 def add(a,b,c=9):
     return a+c+b
-
+   
 add(1,2)
